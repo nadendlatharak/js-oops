05f942f8fcb7be66566849df4749859dc169d060
Nadendla Tharak <54901555+nadendlatharak@users.noreply.github.com>
2022-11-17 17:16:43 +0000

    tharal

--- SOLID/oc.js
+++ SOLID/oc.js
@@ -15,3 +15,14 @@ class Vehicle{
 // let there be an addition of electric functionality
 // modify this class to calculatethe range
 
+class electric extends Vehicle {
+    constructor(eff, fuelcap){
+        this.efficiency = this.efficiency;
+        this.capacity = fuelcap;
+    }
+    calcRange(){
+        return this.efficiency * this.capacity
+    }
+}
+
+var obej = new electric('90', 80)
